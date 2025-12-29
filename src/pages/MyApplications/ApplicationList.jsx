592dc140728e4849920cdef040eb86393c0a8cae
Ayon203008 <paypal572874@gmail.com>
2025-12-29 13:36:57 +0600

    myapplication page added

---
+++ src/pages/MyApplications/ApplicationList.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { use } from "react";
+
+const ApplicationList = ({ myApplicationsPromise }) => {
+  const applications = use(myApplicationsPromise);
+
+  return (
+    <div>
+      <h1 className="text-4xl">Jobs Applied so far : {applications.length}</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {applications.map((application) => (
+              <tr>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={application.company_logo} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{application.github}</div>
+                      <div className="text-sm opacity-50">
+                        {application.title}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationList;
--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import ApplicationList from "./ApplicationList";
+import { Suspense } from "react";
+import { use } from "react";
+import { AuthContext } from "../../Context/AuthContext";
 
+const myApplicationsPromise = (email) => {
+  return fetch(`http://localhost:3000/applications?email=${email}`).then(
+    (res) => res.json()
+  );
+};
 const MyApplications = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { user } = use(AuthContext);
+
+  return (
+    <div>
+      <Suspense fallback={"Loading keep patience"}>
+        <ApplicationList
+          myApplicationsPromise={myApplicationsPromise(user.email)}
+        ></ApplicationList>
+      </Suspense>
+    </div>
+  );
 };
 
 export default MyApplications;
